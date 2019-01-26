c35b8dc3fd170dca1481ea3b0687b5af3c10c2d2
Andrew Lawrence He <he.andrew.mail@gmail.com>
2019-01-26 14:49:59 -0500

    Replace nim-product
    Replace it with my code, which should be a lot more robust

--- content/misc/nim-product.cpp
+++ content/misc/nim-product.cpp
@@ -1,24 +1,20 @@
 /**
- * Author: 
+ * Author: Andrew He
  * Description: Nim Product.
  */
-ll f[64][64]; // initialize to -1
-ll nim(ll x, ll y);
-ll _nim(int x, int y) {
-  if (!x || !y)  return 1ll << (x + y);
-  if (f[x][y] != -1)  return f[x][y];
-  ll ret = 1, e = 1;
-  for (int i = 0; (x >> i) || (y >> i); i++)
-    if (((x ^ y) >> i) & 1)  e <<= (1 << i);
-    else if ((x >> i) & 1)  ret = nim(ret, 3 * (1ll << (1 << i)) / 2);
-  f[x][y] = nim(ret, e);
-  return f[x][y];
+ll _nimProd2[20][20];
+ll nimProd2(int i, int j) {
+  if (_nimProd2[i][j]) return _nimProd2[i][j];
+  if ((i & j) == 0) return _nimProd2[i][j] = 1ll << (i|j);
+  int a = (i&j) & -(i&j);
+  return _nimProd2[i][j] = nimProd2(i ^ a, j) ^ nimProd2((i ^ a) | (a-1), (j ^ a) | ((i|j) & (a-1)));
 }
-ll nim(ll x, ll y) {
-  ll ret = 0;
-  for (int i = 0; x >> i; i++)
+ll nimProd(ll x, ll y) {
+  ll res = 0;
+	for (int i = 0; x >> i; i++)
     if ((x >> i) & 1)
       for (int j = 0; y >> j; j++)
-        if ((y >> j) & 1)  ret ^= _nim(i, j);
-  return ret;
+        if ((y >> j) & 1)
+          res ^= nimProd2(i, j);
+  return res;
 }
