7fc405bb2006cd65539fa55fa3821bae4cf23cc0
Andrew Lawrence He <he.andrew.mail@gmail.com>
2019-01-26 14:00:38 -0500

    Fix nim-product to work on bigger nim values
    It should work on bigger numbers... we should figure out how big

--- content/misc/nim-product.cpp
+++ content/misc/nim-product.cpp
@@ -2,21 +2,22 @@
  * Author: 
  * Description: Nim Product.
  */
-int _nim(int x, int y) {
+ll f[64][64]; // initialize to -1
+ll _nim(int x, int y) {
   if (!x || !y)  return 1 << (x + y);
   if (f[x][y] != -1)  return f[x][y];
-  int ret = 1, e = 1;
-  for (int i = 0; i <= 4; i++)
-    if ((x ^ y) & (1 << i))  e *= (1 << (1 << i));
-    else  if (x & (1 << i))  ret = nim(ret, 3 * (1 << (1 << i)) / 2);
+  ll ret = 1, e = 1;
+  for (int i = 0; (x >> i) || (y >> i); i++)
+    if (((x ^ y) >> i) & 1)  e <<= (1 << i);
+    else if ((x >> i) & 1)  ret = nim(ret, 3 * (1ll << (1 << i)) / 2);
   f[x][y] = nim(ret, e);
   return f[x][y];
 }
-int nim(int x, int y) {
-  int ret = 0;
-  for (int i = 0; i <= 20; i++)
-    if (x & (1 << i))
-      for (int j = 0; j <= 20; j++)
-        if (y & (1 << j))  ret ^= _nim(i, j);
+ll nim(ll x, ll y) {
+  ll ret = 0;
+  for (int i = 0; x >> i; i++)
+    if ((x >> i) & 1)
+      for (int j = 0; y >> j; j++)
+        if ((y >> j) & 1)  ret ^= _nim(i, j);
   return ret;
 }
