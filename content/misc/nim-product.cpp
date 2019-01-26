e730b6b2f5f647e147e83254fd7258e9ef9b8160
Andrew Lawrence He <he.andrew.mail@gmail.com>
2019-01-26 15:04:28 -0500

    Update types
    This works up to 2^64

--- content/misc/nim-product.cpp
+++ content/misc/nim-product.cpp
@@ -2,16 +2,17 @@
  * Author: Andrew He
  * Description: Nim Product.
  */
-ll _nimProd2[20][20];
-ll nimProd2(int i, int j) {
+using ull = uint64_t;
+ull _nimProd2[64][64];
+ull nimProd2(int i, int j) {
   if (_nimProd2[i][j]) return _nimProd2[i][j];
-  if ((i & j) == 0) return _nimProd2[i][j] = 1ll << (i|j);
+  if ((i & j) == 0) return _nimProd2[i][j] = 1ull << (i|j);
   int a = (i&j) & -(i&j);
-  return _nimProd2[i][j] = nimProd2(i ^ a, j) ^ nimProd2((i ^ a) | (a-1), (j ^ a) | ((i|j) & (a-1)));
+  return _nimProd2[i][j] = nimProd2(i ^ a, j) ^ nimProd2((i ^ a) | (a-1), (j ^ a) | (i & (a-1)));
 }
-ll nimProd(ll x, ll y) {
-  ll res = 0;
-	for (int i = 0; x >> i; i++)
+ull nimProd(ull x, ull y) {
+  ull res = 0;
+  for (int i = 0; x >> i; i++)
     if ((x >> i) & 1)
       for (int j = 0; y >> j; j++)
         if ((y >> j) & 1)
