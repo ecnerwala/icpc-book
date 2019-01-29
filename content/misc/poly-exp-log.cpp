d990689bea0ed81c051b8f6eab41b1ed85988c85
Andrew He <he.andrew.mail@gmail.com>
2019-01-28 21:24:08 -0500

    Replace l with v for clarity

--- content/misc/poly-exp-log.cpp
+++ content/misc/poly-exp-log.cpp
@@ -13,14 +13,14 @@ const int mo=998244353,g=3;
    int j=0;
    rep(i,0,n){
      if(i>j)swap(a[i],a[j]);
-     for (int l=n>>1;(j^=l)<l;l>>=1);
+     for (int v=n>>1;(j^=v)<v;v>>=1);
    }
    for (int i=2;i<=n;i<<=1)
      for (int j=0,s=n/i;j<n;j+=i)
-       rep(l,0,i>>1){
-         int t=1ll*a[j+l+(i>>1)]*w[v][s*l]%mo;
-         a[j+l+(i>>1)]=(a[j+l]+mo-t)%mo;
-         a[j+l]=(a[j+l]+t)%mo;
+       rep(v,0,i>>1){
+         int t=1ll*a[j+v+(i>>1)]*w[v][s*v]%mo;
+         a[j+v+(i>>1)]=(a[j+v]+mo-t)%mo;
+         a[j+v]=(a[j+v]+t)%mo;
 } if(v){
      int y=qp(n,mo-2);
      rep(i,0,n)a[i]=1ll*a[i]*y%mo;
